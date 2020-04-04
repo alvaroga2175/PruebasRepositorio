34a67d4caa800fe72ae9e85c376e4e111709baf2
alvaroga2175 <61882458+alvaroga2175@users.noreply.github.com>
2020-04-04 18:03:35 +0200

    Update JavaApplication22.java

--- JavaApplication22/src/javaapplication22/JavaApplication22.java
+++ JavaApplication22/src/javaapplication22/JavaApplication22.java
@@ -20,6 +20,7 @@ public class JavaApplication22 {
         System.out.println("Hola33");
         System.out.println("Hola44");
         System.out.println("Hola55");
+        System.out.println("Hola66");
     }
     
 }
