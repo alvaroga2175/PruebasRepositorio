6b850ca55a4760af4e5dd7c50897674f982a8e63
alvarogonzalez <alvarogonzalez@localhost>
2020-04-04 17:25:32 +0200

    1 prueba

---
+++ JavaApplication22/src/javaapplication33/JavaApplication33.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication33;
+
+/**
+ *
+ * @author alvarogonzalez
+ */
+public class JavaApplication33 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+        System.out.println("Hola22");
+        System.out.println("Hola33");
+    }
+    
+}
